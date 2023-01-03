e485ff37cfa0632286fbfe36711be323e4c3b61b
vignesh8458 <71544638+vignesh8458@users.noreply.github.com>
2023-01-03 19:12:17 +0530

    Update Device_Rename.ps1

--- Device_Rename.ps1
+++ Device_Rename.ps1
@@ -4,12 +4,10 @@ $SerialNumber = (Get-WmiObject -class win32_bios).SerialNumber
 Write-Output "$(date) | Looking up public IP"
 ##$myip=(dig +short myip.opendns.com resolver1.opendns.com)
 $myip=(Invoke-WebRequest -uri "https://api.ipify.org/").Content
-Write-Output "$myip"
+
 $Country= (Invoke-WebRequest -uri https://ipapi.co/$myip/country).Content
 ##$Country= (curl -s https://ipapi.co/$myip/country)
 
 $Computer = "$Country-$SerialNumber"
-
-Write-Output "$Computer"
 
 Rename-Computer -NewName $computer -Force
