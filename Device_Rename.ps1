c8d460432f3761352521e80e191d4bb66caf8b64
vignesh8458 <71544638+vignesh8458@users.noreply.github.com>
2023-02-07 23:08:07 +0530

    Update Device_Rename.ps1

--- Device_Rename.ps1
+++ Device_Rename.ps1
@@ -1,13 +1,51 @@
+$Logfile = "C:\Scripts-Logs\$(get-date -f dd-MM).log"
+
+Function LogWrite
+{
+   Param ([string]$logstring)
+
+   Add-content $Logfile -value $logstring
+}
+
+
 $SerialNumber = (Get-WmiObject -class win32_bios).SerialNumber
 
 ## What is our public IP
-Write-Output "$(date) | Looking up public IP"
-##$myip=(dig +short myip.opendns.com resolver1.opendns.com)
-$myip=(Invoke-WebRequest -uri "https://api.ipify.org/").Content
+LogWrite "$(date) | Looking up public IP"
 
-$Country= (Invoke-WebRequest -uri https://ipapi.co/$myip/country).Content
-##$Country= (curl -s https://ipapi.co/$myip/country)
+$SleepTimer = 2
+
+for ($i=0; $i -le 5 ; $i++) {
+$Request=Invoke-WebRequest -uri "https://api.ipify.org/"
+if ($Request.StatusCode -ne 200) {
+LogWrite "IP LookUp Failed Sleeping for $SleepTimer"
+Start-Sleep -Seconds $SleepTimer
+}
+else {break}
+$SleepTimer = $SleepTimer + 1
+}
+
+$MyIp = $Request.Content
+LogWrite "IP is $MyIp"
+
+
+
+$SleepTimer = 2
+
+for ($i=0; $i -le 5 ; $i++) {
+$Request=Invoke-WebRequest -uri https://ipapi.co/$MyIp/country
+if ($Request.StatusCode -ne 200) {
+LogWrite "Country LookUp Failed Sleeping for $SleepTimer"
+Start-Sleep -Seconds $SleepTimer
+}
+else {break}
+$SleepTimer = $SleepTimer + 1
+}
+
+$Country= $Request.Content
+LogWrite "Country Code is $Country"
 
 $Computer = "$Country-$SerialNumber"
+LogWrite "New Computer name $Computer"
 
 Rename-Computer -NewName $computer -Force
